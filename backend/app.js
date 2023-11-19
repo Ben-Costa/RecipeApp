406a1498a53a4bcc2c570e7f737370b28911bee6
Ben-PC-2 <bc234cc@gmail.com>
2023-11-19 09:44:26 -0800

    Basic routing created

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,24 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// Import routes
+const api1Routes = require('./routes/api/ingredientRoutes');
+const api2Routes = require('./routes/api/userRoutes');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
+// Use imported routes in the application
+app.use('/ingredientRoutes', api1Routes);
+app.use('/userRoutes', api2Routes);
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/RecipeBook', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
